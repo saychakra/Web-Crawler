fff8086884e97355758eb933d973e2efcddf8368
Sayan Chakraborty <saychakra@gmail.com>
2018-12-28 21:43:07 +0530

    added gather_links method

--- spider.py
+++ spider.py
@@ -13,6 +13,7 @@ class Spider:
     queue = set()       # set of all urls in the waiting list
     crawled = set()     # set of all urls in th crawled list
 
+
     # method for creating the initial project directory and the data files within
     @staticmethod # because we are only assigning values to class variables a method was not really necessary
     def boot():
@@ -27,8 +28,48 @@ class Spider:
         Spider.crawled = gen.file_to_set(Spider.crawled_file)
 
 
-    def crawl_page(self):
+    @staticmethod
+    def add_links_to_queue(urls):
         pass
+
+
+    @staticmethod
+    def gather_links(page_url):
+        html_string = ''
+        try:
+            response = urlopen(page_url)
+            # just check if the page contains html and is not something else eg. a pdf or something
+            if response.getheader('Content-Type') == 'text/html': 
+                html_bytes = response.read() # reading the raw bytes from response to html_bytes
+                html_string = html_bytes.decode('utf-8') # decoding the bytes to utf-8 string format
+            finder = LinkFinder(Spider.base_url, page_url)
+            finder.feed(html_string)
+        except:
+            print("Error: Cannot Crawl Page")
+            return set() # our function needs to return a set from where it is called. 
+            # Hence even if anything is not found at least an empty set should be returned
+        # finally returning the set of urls from the LinkFinder.page_links()
+        return finder.page_links()
+
+
+    @staticmethod
+    def update_files():
+        pass
+
+
+    @staticmethod
+    def crawl_page(thread_name, page_url):
+        # making sure we haven't already crawled this page already
+        if page_url not in Spider.crawled:
+            print(thread_name, 'is crawling', page_url)
+            print('Queue ' + str(len(Spider.queue)) + '     |       crawled ' + str(len(Spider.crawled)))
+            Spider.add_links_to_queue(Spider.gather_links(page_url))
+            # remove page_url from the queue and add it to the crawled list
+            Spider.queue.remove(page_url)
+            Spider.crawled.add(page_url)
+            # update the files in the directory
+            Spider.update_files()
+
 
     def __init__(self, project_name, base_url, domain_name):
         # assigning the same project_name, base_url and domain_name for all spider instances created 
@@ -41,4 +82,4 @@ class Spider:
         Spider.crawled_file = project_name + '/crawled.txt'
 
         self.boot() 
-        # self.crawl_page('First Spider', Spider.base_url)
+        self.crawl_page('First Spider', Spider.base_url)
